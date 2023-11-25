863fdb717dc8d3c24158d53a91e863cb4773e71f
strojn <anatolii.strojn@gmail.com>
2023-11-25 16:41:42 +0200

    StAX for create xml-file (push) SAXParser for parse xml-file (pull)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,21 +7,89 @@ import org.xml.sax.helpers.DefaultHandler;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
+// 1. Використовуємо StAX для створення XML-ф-лу (наче може і парсити)
+// 2. Використовуємо SAXParser для парсингу попередньо створеного XML-ф-лу (може ТІЛЬКИ парсити)
+
+//StAX
+        FileOutputStream fileOutputStream;
+        try {
+            fileOutputStream = new FileOutputStream("address_created.xml");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        XMLOutputFactory outputFactory = XMLOutputFactory.newInstance();
+        XMLStreamWriter writer;
+        try {
+//            writer = outputFactory.createXMLStreamWriter(System.out);
+            writer = outputFactory.createXMLStreamWriter(fileOutputStream);
+            writer.writeStartDocument("1.0");
+            writer.writeStartElement("places");
+
+            writer.writeStartElement("address");//address 1 ->
+
+            writer.writeStartElement("city");
+            writer.writeAttribute("size", "big");
+            writer.writeCharacters("Kyiv");
+            writer.writeEndElement();
+
+            writer.writeStartElement("street");
+            writer.writeCharacters("Khreshchatyk");
+            writer.writeEndElement();
+
+            writer.writeStartElement("building");
+            writer.writeCharacters("26");
+            writer.writeEndElement();
+
+            writer.writeEndElement();////address 1 <-
+
+            writer.writeStartElement("address");//address 2 ->
+
+            writer.writeStartElement("city");
+            writer.writeAttribute("size", "medium");
+            writer.writeCharacters("Brovary");
+            writer.writeEndElement();
+
+            writer.writeStartElement("street");
+            writer.writeCharacters("Nezalezhnosti boulevard");
+            writer.writeEndElement();
+
+            writer.writeStartElement("building");
+            writer.writeCharacters("11-A");
+            writer.writeEndElement();
+
+            writer.writeEndElement();////address 2 <-
+
+            writer.writeEndElement();//places
+            writer.writeEndDocument();
+
+            writer.flush();
+            writer.close();
+
+        } catch (XMLStreamException e) {
+            throw new RuntimeException(e);
+        }
+
+
+//SAXParser
         //ім'я ф-лу
-        final String fileName = "address.xml";
+//        final String fileName = "address.xml";
+        final String fileName = "address_created.xml";
 
         SAXParserFactory factory = SAXParserFactory.newInstance();
         SAXParser parser;
         try {
             parser = factory.newSAXParser();
-        } catch (ParserConfigurationException e) {
-            throw new RuntimeException(e);
-        } catch (SAXException e) {
+        } catch (ParserConfigurationException | SAXException e) {
             throw new RuntimeException(e);
         }
 
@@ -63,9 +131,7 @@ public class Main {
         //починаємо аналіз XML ф-лу методом parse()
         try {
             parser.parse(fileName, handler);
-        } catch (SAXException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
+        } catch (SAXException | IOException e) {
             throw new RuntimeException(e);
         }
     }
